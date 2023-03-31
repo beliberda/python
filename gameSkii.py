84135ce616a209cacdb9972f1efabfa7e84d1f8b
Anton <90580311+beliberda@users.noreply.github.com>
2023-03-31 17:47:19 +1000

    Update gameSkii.py

--- gameSkii.py
+++ gameSkii.py
@@ -8,13 +8,13 @@ yeti = 2
 neighbour = 3
 totalDistance = 400
 
-def CheckSkill(enemy, hp, speed):
+def CheckSkill(enemy, hp, speed, num):
         checkSkill = int(input(f'У вас на пути встал {enemy}, куда свернуть? \n1. Влево \n2. Вправо \n'))
         randomNumber = randint(1,2)  
         if (checkSkill == randomNumber):
             print(f'Ты избежал {enemy} на скорости ', speed, ' км/сек' )
         else:
-            hp -= speed
+            hp -= num
             print(f'ты столкнулся с {enemy}, осталось: {hp} хп')
             speed = 0
 
@@ -28,11 +28,11 @@ while True:
         break
     randomNumber = randint(0,10)   
     if randomNumber == yeti:
-        CheckSkill('Йети', hp, speed)
+        CheckSkill('Йети', hp, speed,10)
     elif randomNumber == neighbour:
-        CheckSkill('Сосед', hp, speed)
+        CheckSkill('Сосед', hp, speed,20)
     elif randomNumber == rock:
-        CheckSkill('Камень', hp, speed)
+        CheckSkill('Камень', hp, speed,5)
 
     totalDistance -= speed
     speed+=1
